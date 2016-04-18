22c926bd0118ac8f132c307c76e167303886f473
Dimamasj <dimamasj@gmail.com>
2016-04-18 18:25:44 +0300

    add film info block, comments + small css fix

--- app/dist/js/build.min.js
+++ app/dist/js/build.min.js
@@ -6,12 +6,15 @@
         requestsList = {                            //links for API request
             searchPeople: 'searchPeople?keyword',
             getPeopleDetail: 'getPeopleDetail?peopleID',
-            getFilm: 'getFilm?filmID'
+            getFilm: 'getFilm?filmID',
+            getReviews: 'getReviews?filmID'
+
         },
         asideDesc = $('.aside-desc'),
         avatarBlock = $('.avatar-block'),
         actorInfo = $('.actor-info'),
-        career = $('.career');
+        career = $('.career'),
+        commentBlock = $('aside .comments');
 
 
     /**
@@ -106,14 +109,25 @@
 
                 //career.append(preloader);
 
-                getNumArray(data.filmography);
-                getNumArray(data.gallery);
-                getNumArray(data.triviaData);
-                getNumArray(data.generalFilms);
+                if (data.filmography) {
+                    getNumArray(data.filmography);
+                }
 
+
+                if (data.gallery) {
+                    getNumArray(data.gallery);
+                }
+
+                if (data.triviaData) {
+                    getNumArray(data.triviaData);
+                }
+
+                if (data.generalFilms) {
+                    getNumArray(data.generalFilms);
+                }
 
                 for (var j = 0; j < personDataArray.length; j++) {
-                    console.log(personDataArray[j]);
+                    //console.log(personDataArray[j]);
                     career.find('a[data-link-type="' + personDataArray[j] + '"] .count').html(getNumArray(data[personDataArray[j]]));
                 }
             });
@@ -121,61 +135,184 @@
     });
 
 
-    var $filmsList = $('.films');
+    var $mainList = $('.list');
     $('.aside-list li a').click(function (e) {
         e.preventDefault();
         var $this = $(this),
             $dataLink = $this.data('link-type');
 
-        console.log ($dataLink);
-        console.log(personInfo[$dataLink]);
+        $('.aside-list li').removeClass('active');
+        $this.parent().addClass('active');
 
-        $filmsList.append(preloader);
-        $filmsList.html('');
-        for (var i = 0; i < personInfo[$dataLink][0].length; i++) {
-            //getJson('getFilm', personInfo[$data][0][i].filmID, function (data) {
-            //    console.log(data);
+        $mainList.append(preloader);
+        $mainList.html('').removeClass('trivia-data');
 
-            $filmsList.append('' +
+        if ($dataLink === 'filmography') {
+            getFilmography(personInfo[$dataLink][0]);
+        } else if ($dataLink === 'generalFilms') {
+            console.log(personInfo[$dataLink]);
+            getGeneralFilms(personInfo[$dataLink]);
+        } else if ($dataLink === 'gallery') {
+            getGallery(personInfo[$dataLink]);
+        } else if ($dataLink === 'triviaData') {
+            getTriviaData(personInfo[$dataLink]);
+        }
+        $mainList.find('.load').remove();
+    });
+
+
+    /**
+     * Get filmography list
+     * @param obj
+     */
+    function getFilmography(obj) {
+        console.log(obj);
+        for (var i = 0; i < obj.length; i++) {
+            $mainList.append('' +
                 '<div class="film">' +
-                '<a href="#" data-film-id="' + personInfo[$dataLink][0][i].filmID + '">' +
-                '<img src="http://st.kp.yandex.net/images/film_big/' + personInfo[$dataLink][0][i].filmID + '.jpg" alt="">' +
+                '<a href="#" data-film-id="' + obj[i].filmID + '">' +
+                '<img src="http://st.kp.yandex.net/images/film_big/' + obj[i].filmID + '.jpg" alt="">' +
                 '</a>' +
                 '</div>' +
                 '');
-            //})
         }
-        avatarBlock.find('.load').remove();
-    });
+    }
+
+    /**
+     * Get General films list
+     * @param obj
+     */
+    function getGeneralFilms(obj) {
+        console.log(obj);
+        for (var i = 0; i < obj.length; i++) {
+            $mainList.append('' +
+                '<div class="film">' +
+                '<a href="#" data-film-id="' + obj[i].filmID + '">' +
+                '<img src="http://st.kp.yandex.net/images/film_big/' + obj[i].filmID + '.jpg" alt="">' +
+                '</a>' +
+                '</div>' +
+                '');
+        }
+    }
+
+    /**
+     * Get gallery about actor
+     * @param obj
+     */
+    function getGallery(obj) {
+        console.log(obj);
+        for (var i = 0; i < obj.length; i++) {
+            $mainList.append('' +
+                '<div class="gallery">' +
+                '<a href="#" data-gallery-item="http://st.kp.yandex.net/images/' + obj[i].preview + '">' +
+                '<img src="http://st.kp.yandex.net/images/' + obj[i].preview + '" alt="">' +
+                '</a>' +
+                '</div>' +
+                '');
+        }
+    }
+
+    /**
+     * Get trivia data about actor
+     * @param obj
+     */
+    function getTriviaData(obj) {
+        console.log(obj);
+        for (var i = 0; i < obj.length; i++) {
+            $mainList.addClass('trivia-data').append('' +
+                '<p>' + obj[i] + '</p>' +
+                '');
+        }
+    }
 
 
-    $filmsList.on('click', 'a', function (e) {
+    $mainList.on('click', 'a', function (e) {
         e.preventDefault();
         var $this = $(this),
-            $filmId = $this.data('film-id');
+            $filmId = $this.data('film-id'),
+            filmInfoColl = $('.list .film-info'),
+            parentElem = $this.closest('.film'),
+            leftOffset = parentElem.position().left,
+            mainContentOffset = $mainList.position().left,
+            result = getElementsPerRow(parentElem);
 
-        getElementsPerRow($this.closest('.film'));
 
+        getJson('getFilm', $filmId, function (data) {
+            console.log(data);
+
+            filmInfoColl.html('').hide();
+
+            var infoToShow = '<div class="film-poster">' +
+                '<img src="http://st.kp.yandex.net/images/' + data.posterURL.replace('90_', '360_') + '" alt="">' +
+                '</div><div class="film-desc">' +
+                '<ul class="film-desc__list">' +
+                '<li><span class="desc-title">Year: </span><span class="desc-text">' + data.year + '</span></li>' +
+                '<li><span class="desc-title">Country: </span><span class="desc-text">' + data.country + '</span></li>' +
+                '<li><span class="desc-title">Slogan: </span><span class="desc-text">' + data.slogan + '</span>' +
+                '</li>' +
+                '<li><span class="desc-title">Genre: </span><span class="desc-text">' + data.genre + '</span></li>' +
+                '<li><span class="desc-title">Rating: </span><span class="desc-text">' + data.ratingData.rating + '</span></li>' +
+                '<li><span class="desc-title">Film length: </span><span class="desc-text">' + data.filmLength + '</span></li>' +
+                '</ul>' +
+                '<p class="description">' + data.description + '</p>' +
+                '<div class="read-more">' +
+                '<a class="read-more__link" href="#">Read full description</a>' +
+                '</div>' +
+                '</div>' +
+                '<span class="arrow" style="left: ' + (leftOffset - mainContentOffset) + 'px"></span>' +
+                '<span class="close-info"><i class="fa fa-times" aria-hidden="true"></i></span>';
+
+            if (result.next().hasClass('film-info')) {
+                result.next().html(infoToShow).show();
+            } else {
+                result.after('<div class="film-info film-info--short"></div>');
+                result.next().html(infoToShow).show();
+            }
+            $('body').animate({scrollTop: parentElem.position().top}, 'slow');
+        });
+
+
+        getJson('getReviews', $filmId, function (data) {
+            console.log(data);
+            var reviewsList = data.reviews;
+
+            if (reviewsList) {
+                commentBlock.html('');
+                for (var i = 0; i < 10; i++) {
+                    commentBlock.append('' +
+                        '<article class="' + reviewsList[i].reviewType + '">' +
+                        '<header>' +
+                        '<img src="http://st.kp.yandex.net/images/' + reviewsList[i].reviewAutorImageURL + '" alt="' + data.reviews[i].reviewAutor + '">' +
+                        '<h4>' + reviewsList[i].reviewAutor + '</h4>' +
+                        '</header>' +
+                        '<p>' + reviewsList[i].reviewDescription + '</p>' +
+                        '<footer>' + reviewsList[i].reviewData + '</footer>' +
+                        '</article>')
+                }
+            } else {
+                commentBlock.html('No reviews');
+            }
+        })
     });
 
+
     function getElementsPerRow(elem) {
-        console.log(elem);
+        var $this = elem;
+        var $elemOffset = $this.offset().top;
 
-        var $elemOffset = elem.offset().top;
-
-        //for (var i = 0; i < 5; i++) {
-        //    if (){
-        //
-        //    }
-        //}
-
-
-        console.log($elemOffset);
-        console.log(elem.next().offset().top);
-
-
-
-
+        for (var i = 1; i < 5; i++) {
+            if ($this.next()) {
+                console.log($this.next());
+                if ($this.next().offset().top === $elemOffset) {
+                    $this = $this.next();
+                }
+                else {
+                    return $this;
+                }
+            } else {
+                return $this;
+            }
+        }
     }
 
 
@@ -198,15 +335,10 @@
     }
 
 
-//function normalizeObj (data) {
-//    if (list[0] instanceof Array) {
-//        list.forEach(function (n) {
-//            $counter += n.length;
-//        });
-//    } else {
-//        $counter = list.length;
-//    }
-//    return $counter;
-//}
-
+    /**
+     * Close Film Description
+     */
+    $mainList.on('click', '.close-info', function () {
+        $(this).closest('.film-info').hide();
+    });
 })();
